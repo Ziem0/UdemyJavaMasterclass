9f78226aa7ad4a4cfdf302f18253bc0ef3e9dfc1
Ziem0 <andrzejewski.ziemo@gmail.com>
2018-01-08 18:39:28 +0100

    add VehicleChallange package

---
+++ src/VehicleChallange/Outlander.java
@@ -0,0 +1,31 @@
+package VehicleChallange;
+
+/**
+ * Created by dev on 8/3/15.
+ */
+public class Outlander extends Car {
+    private int roadServiceMonths;
+
+    public Outlander(int roadServiceMonths) {
+        super("Outlander", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate) {
+        int newVelocity = getCurrentVelocity() + rate;
+        if (newVelocity == 0) {
+            stop();
+            changeGear(1);
+        } else if (newVelocity > 0 && newVelocity <= 10) {
+            changeGear(1);
+        } else if (newVelocity > 10 && newVelocity <= 20) {
+            changeGear(2);
+        } else if (newVelocity > 20) {
+            changeGear(3);
+        }
+
+        if (newVelocity != getCurrentVelocity()) {
+            changeVelocity(newVelocity, getCurrentDirection(),rate);
+        }
+    }
+}
